ca708548c666e2e456f401dd7c4a199fdeee97e9
Md. Abdul Motin <motiniu92@gmail.com>
2019-12-31 18:44:48 +0600

    firstCommit

---
+++ Zhoomprass/app/src/main/java/com/zhomprass/zhoomprasslimited/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zhomprass.zhoomprasslimited.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zhomprass.zhoomprasslimited.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
