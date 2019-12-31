c9d5aaa078c31fa346e463ee8b87239f4e806b47
Md. Abdul Motin <motiniu92@gmail.com>
2020-01-01 03:32:39 +0600

    PreviousProjects

---
+++ ecommerce/app/src/main/java/com/bahadur/bahadurapp/fragments/Intro_Slide_4.java
@@ -0,0 +1,24 @@
+package com.bahadur.bahadurapp.fragments;
+
+import android.support.annotation.Nullable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bahadur.bahadurapp.R;
+
+
+public class Intro_Slide_4 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.intro_slide_4, container, false);
+    
+        return rootView;
+    }
+    
+}
+
